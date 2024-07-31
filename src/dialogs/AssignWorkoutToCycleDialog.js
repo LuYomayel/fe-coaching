e73f6f89481ee00e7a7e7024765305660cd28c39
LuYomayel <l.yomayel@mgail.com>
2024-07-31 17:20:56 +1000

    dayofweek 1-7

--- src/dialogs/AssignWorkoutToCycleDialog.js
+++ src/dialogs/AssignWorkoutToCycleDialog.js
@@ -37,13 +37,13 @@ const AssignWorkoutToCycleDialog = ({ visible, onHide, cycleId, clientId, setRef
 
 
   const daysOfWeek = [
-    { label: 'Sunday', value: 0 },
     { label: 'Monday', value: 1 },
     { label: 'Tuesday', value: 2 },
     { label: 'Wednesday', value: 3 },
     { label: 'Thursday', value: 4 },
     { label: 'Friday', value: 5 },
     { label: 'Saturday', value: 6 },
+    { label: 'Sunday', value: 7 },
   ];
 
   const handleAssign = async () => {
